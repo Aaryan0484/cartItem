308df093fd93da042415e57e72ee4aa12bcb91d1
Aaryan0484 <aaryankumarak18@gmail.com>
2024-01-06 01:05:14 +0530

    shop page is created and 7 product is also added in the same

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {PRODUCTS} from '../../products'
+import {Product} from './product'
+import './shop.css'
 function shop() {
   return (
-    <div>shop</div>
+    <div className="shop">
+    <div className="shopTitle">
+      <h1>ALL IN ONE</h1>
+    </div>
+
+    <div className="products">
+      {PRODUCTS.map((product) => (
+        <Product data={product} />
+      ))}
+    </div>
+  </div>
   )
 }
 
---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from './images/1.png';
+import product2 from './images/2.png';
+import product3 from './images/3.png';
+import product4 from './images/4.png';
+import product5 from './images/5.webp';
+import product6 from './images/6.webp';
+import product7 from './images/7.webp';
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Iphone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro",
+    price: 1499.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon Camera",
+    price: 1299.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Denim Jacket",
+    price: 149.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "SweatShirt",
+    price: 119.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Half Sleeves Shirt",
+    price: 49.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "HAT",
+    price: 39.0,
+    productImage: product7,
+  }
+];
