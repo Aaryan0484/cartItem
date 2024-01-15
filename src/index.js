3b34def665ab97affae2b4297a67b00ac2ef15b7
Aaryan0484 <aaryankumarak18@gmail.com>
2024-01-15 13:57:20 +0530

    Create gh-pages branch for deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAp_L2adCcFMYg8hngdKkMQDox1JrM4dSo",
+  authDomain: "cart-82316.firebaseapp.com",
+  projectId: "cart-82316",
+  storageBucket: "cart-82316.appspot.com",
+  messagingSenderId: "182180872621",
+  appId: "1:182180872621:web:9dd4be00c8bb5eef35e124"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
